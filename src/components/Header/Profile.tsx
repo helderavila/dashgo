2722463adb30aa64cf28f9c7c62cd34253124d82
Helder Avila <helderavila2016@gmail.com>
2021-07-31 13:02:20 -0300

    🎨 responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Helder Avila</Text>
-        <Text color="gray.300" fontSize="small">oi@helderavila.com.br</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Helder Avila</Text>
+          <Text color="gray.300" fontSize="small">oi@helderavila.com.br</Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Helder Avila"
