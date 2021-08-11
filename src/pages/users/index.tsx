6d2f246b4fdbe2785e8dd4695d7117361a638ecf
Helder Avila <helderavila2016@gmail.com>
2021-08-11 19:46:29 -0300

    feat: show the refetch status and axios setup

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,11 +5,12 @@ import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+    const { data } = await api.get('users')
+
     const users = data.users.map(user => ({
       id: user.id,
       name: user.name,
@@ -41,6 +42,7 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
